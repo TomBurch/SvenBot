83c1de6f257455837eecaac3d0d8fcd9d5f49251
Tom Burch <thomas.burch1306@gmail.com>
2021-03-10 23:12:46 +0000

    Add ping

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def ping():
+    if request.json["type"] == 1:
+        return jsonify({
+            "type": 1
+        })
 
 @app.route('/abc/')
 def hello_world():
