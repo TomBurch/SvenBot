8d48d9dd3aa75f73e2654623e1d6c334438a72cf
TomBurch <thomas.burch1306@gmail.com>
2021-03-10 23:41:55 +0000

    Add request verification

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+import os
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+
 from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+verify_key = VerifyKey(bytes.fromhex(DISCORD_TOKEN))
+
 app = Flask(__name__)
+
+def verifyRequest(request):
+    try:
+        signature = request.headers["X-Signature-Ed25519"]
+        timestamp = request.headers["X-Signature-Timestamp"]
+        body = request.data
+        return True
+    except:
+        abort(401, 'invalid request signature')
+        return False
 
 @app.route('/', methods=['POST'])
 def ping():
+    if (not verifyRequest(request)): return
+
     if request.json["type"] == 1:
         return jsonify({
             "type": 1
@@ -10,6 +32,8 @@ def ping():
 
 @app.route('/abc/')
 def hello_world():
+    if (not verifyRequest(request)): return
+    
     return "Hello, World!"
 
 if __name__ == "__main__":
