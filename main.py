ab96ce91bbe9cd4b32c446716c29050a05d37038
TomBurch <thomas.burch1306@gmail.com>
2021-03-24 19:10:00 +0000

    Try enabling debugging

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+try:
+  import googleclouddebugger
+  googleclouddebugger.enable(
+    breakpoint_enable_canary=True
+  )
+except ImportError:
+  pass
+
 import os
 import logging
 from datetime import datetime, timedelta
