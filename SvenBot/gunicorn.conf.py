44a66cf407dfdb8e0ae13913532e5b7f26ed6108
TomBurch <thomas.burch1306@gmail.com>
2021-12-28 15:31:14 +0000

    Update gunicorn.conf.py

--- SvenBot/gunicorn.conf.py
+++ SvenBot/gunicorn.conf.py
@@ -11,5 +11,10 @@ def on_starting(server):
     gunicorn_logger.info(f"Starting Server {server}")
 
 
+def pre_request(worker, req):
+    worker.log.debug("%s %s" % (req.method, req.path))
+    gunicorn_logger.info("%s %s" % (req.method, req.path))
+
+
 def post_request(worker, req, environ, resp):
     gunicorn_logger.info(f"Postreq: {resp}")
