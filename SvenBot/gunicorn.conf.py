99e2cc6afeef21b21e6f431a6171e0604f2a4759
TomBurch <thomas.burch1306@gmail.com>
2021-12-28 15:17:03 +0000

    Create gunicorn.conf.py

---
+++ SvenBot/gunicorn.conf.py
@@ -0,0 +1,11 @@
+from SvenBot.main import app
+
+bind = '0.0.0.0:8000'
+workers = 1
+loglevel = "info"
+worker_class = "uvicorn.workers.UvicornWorker"
+
+
+# Server Hooks
+def on_starting(server):
+    app.logger.info(f"Starting Server {server}")
