1ebc05eea8d15dbe610131e3871ba906f74403f3
TomBurch <thomas.burch1306@gmail.com>
2021-12-28 15:42:46 +0000

    Update gunicorn.conf.py

--- SvenBot/gunicorn.conf.py
+++ SvenBot/gunicorn.conf.py
@@ -5,6 +5,7 @@ workers = 1
 loglevel = "info"
 worker_class = "uvicorn.workers.UvicornWorker"
 accesslog = "./access.txt"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 
 # Server Hooks
